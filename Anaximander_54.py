fffb928c0111e0ff9dca935f2d17e1b34f949ec2
forensic-knight <work@rms2.de>
2017-02-20 10:01:03 +0100

    Add files via upload
     * Fixed problem with leading zeros in MNC Records
     * Reports "No matching record found"

--- Anaximander_54.py
+++ Anaximander_54.py
@@ -1,8 +1,14 @@
-# Anaximander v020160830
-# Python 2.7
-# Written by Matt Edmondson
-# Thanks to Ed Michael & Douglas Kein
-
+#!/usr/bin/python
+# Python:        2.7
+# Filename:      Anaximander_54.py
+# Purpose:       Enrich UFED XML Report files with celltower geo data
+# Authors:       Matt Edmondson
+#                Thanks to Ed Michael & Douglas Kein
+# Modified by:   Forensic Knight :)
+# Version:       0.9 2017-02-20
+# Bug-Reports:   see https://github.com/forensic-knight/Anaximander and https://github.com/azmatt/Anaximander
+# License:       This file is licensed under the GPL v3.
+################################################################################
 
 import xml.etree.ElementTree as ET
 import sqlite3
@@ -29,7 +35,7 @@ tree = ET.parse(tgtFile)
 root = tree.getroot()
 x = 0
 for child in root[6][0]:
-
+        print "############################"
 	varTimestamp = root[6][0][x][2][0].text
 	#print "Timestamp: %s" % varTimestamp
 	varPackageType = root[6][0][x][3][0].text
@@ -40,6 +46,10 @@ for child in root[6][0]:
 	#print "MCC: %s" % varMcc
 	varMnc = root[6][0][x][6][0].text
 	#print "MNC: %s " % varMnc
+        # UFED delivers some MNCs with leading zeros!
+        if varMnc[0] == '0':
+           print "[i] Removed leading zero of current MNC!"
+           varMnc = varMnc.lstrip('0');
 	varLac = root[6][0][x][7][0].text
 	#print "LAC: %s" % varLac
 	varCid = root[6][0][x][8][0].text
@@ -51,24 +61,28 @@ for child in root[6][0]:
 	db = sqlite3.connect('cellTowers.sqlite')
 	cursor = db.cursor()
 	cursor.execute("select * from towers where mcc = '%s' and net = '%s' and area = '%s' and cell = '%s' limit 1" % (varMcc, varMnc, varLac, varCid) )
+        r = 0
 	for row in cursor:
+                r += 1
 		try:
 			varLon = row[6]
 			#print "Lon: %s" % varLon
 			varLat = row[7]
 			#print "Lat: %s " % varLat
-			print "[+] Cont: (Lon: %s, Lat: %s)" % (varLon, varLat)
+			print "[+] Enrich: (Lon: %s, Lat: %s)" % (varLon, varLat)
 			
 			
 			kml_contents = " <Placemark>\n <TimeStamp><when>"+ varTimestamp +"</when></TimeStamp> <name>" + str(x)+ "</name>\n <description> <p><b>Cell ID:</b> " + varCid + "</p><p><b>Date:</b> " + varTimestamp + "</p></description>\n <Point>\n <coordinates>" + varLon + "," + varLat + ",0 </coordinates>\n </Point>\n </Placemark>\n"
 			z.write(kml_contents)
 		except:
-			print "[!] Error with the following record (MCC: %s, MNC: %s, LAC: %s, CID: %s)!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" % (varMcc, varMnc, varLac, varCid)
-	
-	print "############################"
+			print "[!] Error with the following record (MCC: %s, MNC: %s, LAC: %s, CID: %s)!" % (varMcc, varMnc, varLac, varCid)
+        if r == 0:
+           print "[!] Error: No matching record found!"
 	x =x + 1
 	
 cursor.close
+print
+print "############################ Summary ############################"
 print "\n%s Records Parsed" % x
 
 ######Finalizing and Closing KML############
